b502223d3ef434de4e8d464414f9f23d5122cf3b
bareb0nes <97035845+bareb0nes@users.noreply.github.com>
2022-01-15 15:37:19 +0100

    Create codwars 4 pirates.py

---
+++ codwars 4 pirates.py
@@ -0,0 +1,11 @@
+def cannons_ready(gunners):
+    problem = 0
+    for yell in gunners.values():
+        if yell != "aye":
+            problem += 1
+    if problem == 1:
+        return "Shiver me timbers!"
+    else:
+        return "Fire!"
+                     
+  # Fire! or Shiver me timbers!
